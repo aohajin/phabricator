687bb546770860166d71ffa9fdcfa74534f1feb3
怨念 <maohaijian@xiimoon.com>
2019-03-07 15:37:10 +0800

    use nginx from leap-15.0-oss specificly

--- baseline/setup.sh
+++ baseline/setup.sh
@@ -5,8 +5,8 @@ set -x
 
 # Add repositories
 # zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/php/openSUSE_Leap_15.0/ php
-zypper --non-interactive ar https://download.opensuse.org/update/leap/42.3/oss/ update
-zypper --non-interactive ar https://download.opensuse.org/update/leap/15.1/oss/ updata1
+zypper --non-interactive ar https://download.opensuse.org/update/leap/42.3/oss/ openSUSE-Leap-42.3-Update-Oss
+zypper --non-interactive ar https://download.opensuse.org/update/leap/15.1/oss/ openSUSE-Leap-15.1-Update-Oss
 zypper --non-interactive ar https://download.opensuse.org/repositories/devel:/libraries:/c_c++/openSUSE_Leap_15.0/ exprc
 zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/languages:/python/openSUSE_Leap_15.0/ python
 
@@ -19,8 +19,10 @@ zypper --gpg-auto-import-keys --non-interactive in --force-resolution git
 # Add SCM package for other tools (Subversion, Mercurial)...
 # zypper --non-interactive ar http://download.opensuse.org/repositories/devel:/tools:/scm/openSUSE_Leap_42.3/ scm
 
+# nginx from openSUSE-Leap-42.3-Update-Oss conflict with git, so use specificly Leap-15.0-Oss version
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution --repo repo-oss nginx
 # Install requirements
-zypper --gpg-auto-import-keys --non-interactive in --force-resolution nginx php5-fpm php5-mbstring php5-mysql php5-curl php5-pcntl php5-gd php5-openssl php5-ldap php5-fileinfo php5-posix php5-json php5-iconv php5-ctype php5-zip php5-sockets which nodejs8 ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php5-xmlwriter php5-opcache ImageMagick postfix glibc-locale
+zypper --gpg-auto-import-keys --non-interactive in --force-resolution php5-fpm php5-mbstring php5-mysql php5-curl php5-pcntl php5-gd php5-openssl php5-ldap php5-fileinfo php5-posix php5-json php5-iconv php5-ctype php5-zip php5-sockets which nodejs8 ca-certificates ca-certificates-mozilla ca-certificates-cacert sudo subversion mercurial php5-xmlwriter php5-opcache ImageMagick postfix glibc-locale
 
 # Build and install APCu
 zypper --non-interactive install --force-resolution autoconf automake binutils cpp gcc glibc-devel libasan0 libatomic1 libgomp1 libitm1 libltdl7 libmpc3 libpcre16-0 libpcrecpp0 libpcreposix0 libstdc++-devel libstdc++48-devel libtool libtsan0 libxml2-devel libxml2-tools linux-glibc-devel m4 make ncurses-devel pcre-devel php5-devel php5-pear php5-zlib pkg-config readline-devel tack xz-devel zlib-devel
